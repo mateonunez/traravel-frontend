8080f5ba58dc348ddf2a265c8caa6d37e7c4c6a2
mateonunez <mateonunez95@gmail.com>
2022-02-18 18:33:21 +0100

    style: add default layout and base style

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex items-center justify-center w-screen h-screen bg-gray-200">
+    <nuxt />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
