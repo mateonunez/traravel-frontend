667e7152f4b787db249d1c3622822fe2b7d685a5
mateonunez <mateonunez95@gmail.com>
2022-02-19 12:49:05 +0100

    feat(Auth): add basic login screen

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="flex flex-col justify-center max-w-2xl py-20 mx-8 sm:mx-auto fit">
     <!-- Welcome messages -->
-    <div class="flex flex-col">
-      <h1 class="text-3xl">Bentornato!</h1>
-      <span className="-mt-4 text-sm">Effettua l'accesso per proseguire!</span>
+    <div class="flex flex-col max-w-md">
+      <h1 class="font-bold text-5xl">Bentornato!</h1>
+      <span className="-mt-1 text-xs">
+        Non perderti nessuna delle nostre avventure. Fai subito il check-in...
+        login!
+      </span>
     </div>
 
     <!-- Login form -->
-    <div class="flex flex-col mt-10">
+    <form class="flex flex-col mt-8" @submit="handleLogin">
       <!-- Email input -->
       <div class="relative mb-5">
         <span class="font-bold">Email</span>
         <label>
           <input
+            v-model="email"
             type="email"
+            placeholder="Inserisci la tua email"
             :class="
               cn(
                 'w-full py-2 pl-12 pr-2 transition ease-in-out border border-solid rounded-lg focus:border-blue-500 focus:outline-none',
-                !validEmail ? 'border-red-500' : 'border-gray-300 '
+                !validEmail && 'border-red-500'
               )
             "
           />
@@ -32,11 +37,13 @@
         <span class="font-bold">Password</span>
         <label>
           <input
+            v-model="password"
             type="password"
+            placeholder="Inserisci la tua password"
             :class="
               cn(
                 'w-full py-2 pl-12 pr-2 transition ease-in-out border border-solid rounded-lg focus:border-blue-500 focus:outline-none',
-                !validEmail ? 'border-red-500' : 'border-gray-300 '
+                !validPassword && 'border-red-500'
               )
             "
           />
@@ -48,23 +55,35 @@
 
       <!-- Lost Password -->
       <div class="flex flex-row justify-end mt-1 mb-8">
-        <button>
-          <span class="text-sm text-teal-600 cursor-pointer">
+        <button class="cursor-pointer">
+          <span class="text-sm text-orange-600 hover:text-orange-800">
             Dimenticato la Password?
           </span>
         </button>
       </div>
 
       <!-- Submit button -->
-      <div class="flex flex-row mt-auto">
+      <div class="flex flex-row mt-auto justify-center items-center">
         <button
+          :disabled="buttonDisabled"
           type="submit"
-          class="w-full px-4 py-2 font-bold text-white bg-green-500 rounded-lg"
+          :class="
+            cn(
+              'w-full px-4 py-2 font-bold rounded-lg flex justify-center',
+              buttonDisabled ? 'bg-slate-300' : 'bg-red-500 hover:bg-red-700'
+            )
+          "
         >
-          Accedi
+          <Spinner v-if="loading" class="text-red-700" color="pink-500" />
+          <span
+            v-else
+            :class="buttonDisabled ? 'text-red-700' : 'text-gray-200'"
+          >
+            Accedi
+          </span>
         </button>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 <script>
@@ -75,15 +94,47 @@ export default {
 
   components: {
     MailIcon: () => import('@/components/icons/Mail.vue'),
-    LockClosedIcon: () => import('@/components/icons/LockClosed.vue')
+    LockClosedIcon: () => import('@/components/icons/LockClosed.vue'),
+    Spinner: () => import('@/components/common/Spinner.vue')
   },
 
   data: () => ({
-    validEmail: true
+    email: null,
+    password: null,
+    loading: false
   }),
 
+  computed: {
+    validEmail() {
+      return (
+        this.email === null || // prevent error at first render
+        (this.email?.length > 0 &&
+          /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(this.email))
+      )
+    },
+    validPassword() {
+      return (
+        this.password === null || // prevent error at first render
+        (this.password?.length > 0 &&
+          /^(?=.*[a-zA-Z])(?=.*[0-9])/.test(this.password))
+      )
+    },
+    buttonDisabled() {
+      return !this.validEmail || !this.validPassword || this.loading
+    }
+  },
+
   methods: {
-    cn
+    cn,
+    handleLogin(e) {
+      e.preventDefault()
+
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        console.log(this.email, this.password)
+      }, 1000)
+    }
   }
 }
 </script>
