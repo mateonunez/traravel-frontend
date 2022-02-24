e2c59f1bcc78778f3cb641ef62b43ac1a8b7f74e
mateonunez <mateonunez95@gmail.com>
2022-02-24 17:54:20 +0100

    feat(Travel): create simple show page

---
+++ components/travels/TravelShow.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="w-full h-full px-6 py-5 text-white bg-red-500 bg-opacity-90">
+    <div class="flex flex-col my-8 md:w-2/3 xl:w-1/3 md:mx-auto">
+      <!-- Description -->
+      <div class="flex flex-row justify-center">
+        <div class="w-full p-4 text-gray-800 bg-white rounded-lg shadow">
+          <div class="mb-2">
+            <div class="h-3 text-3xl text-left text-gray-600">“</div>
+            <p class="px-4 text-sm text-center text-gray-600">
+              {{ travel.description }}
+            </p>
+            <div class="h-3 text-3xl text-right text-gray-600">”</div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Tours -->
+      <div
+        class="flex flex-col justify-center mt-10 bg-white rounded-lg text-slate-900"
+      >
+        <!-- Tours -->
+        <div
+          v-for="(tour, index) in travel.tours"
+          :key="index"
+          class="flex flex-row items-center justify-start p-3 my-2"
+        >
+          <TourCard :tour="tour" :travel="travel" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { currencyForHumans } from '~/lib/filters/currency'
+import { dateForHumans } from '~/lib/filters/date'
+
+export default {
+  name: 'TravelShow',
+
+  filters: {
+    currencyForHumans,
+    dateForHumans
+  },
+
+  components: {
+    TourCard: () => import('~/components/tours/TourCard.vue')
+  },
+
+  props: {
+    travel: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/travels/_slug/index.vue
+++ pages/travels/_slug/index.vue
@@ -1,9 +1,71 @@
 <template>
-  <div>Show Travel slug</div>
+  <div class="flex flex-col w-full h-full sm:mx-auto">
+    <!-- Navigators -->
+    <div class="flex flex-row justify-between mt-7 mx-7">
+      <!-- Left Navigator -->
+      <Navigator @click="$router.go(-1)">
+        <ArrowLeft
+          class="w-8 h-8 transition duration-300 transform text-slate-800 hover:scale-110"
+        />
+      </Navigator>
+
+      <!-- Cross Navigator -->
+      <Navigator @click="$router.push('/travels')">
+        <Cross
+          class="w-8 h-8 transition duration-300 transform text-slate-800 hover:rotate-90"
+        />
+      </Navigator>
+    </div>
+
+    <!-- Content -->
+    <div class="flex flex-col w-full px-4 py-8 mx-auto mt-20 md:w-1/2">
+      <!-- Title -->
+      <div class="flex flex-row items-center justify-between">
+        <div class="flex flex-col w-full">
+          <h1 class="text-2xl font-bold leading-none text-slate-800">
+            {{ travel.name }}
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <!-- Travel Edit Create -->
+    <div class="flex flex-col w-full sm:mx-auto">
+      <TravelShow :travel="travel" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TravelShowPage'
+  name: 'TravelShowPage',
+
+  components: {
+    Navigator: () => import('~/components/common/Navigator'),
+    ArrowLeft: () => import('~/components/icons/ArrowLeft'),
+    Cross: () => import('~/components/icons/Cross'),
+    TravelShow: () => import('~/components/travels/TravelShow')
+  },
+
+  async fetch() {
+    try {
+      const { slug } = this.$route.params
+
+      await this.$store.dispatch('travels/show', slug)
+    } catch (e) {
+      console.error(e)
+      this.$nuxt.error({ statusCode: 404, message: 'Travel not found' })
+    }
+  },
+
+  computed: {
+    travel() {
+      return this.$store.getters['travels/getEntity']
+    }
+  },
+
+  mounted() {
+    console.log(this.travel)
+  }
 }
 </script>
